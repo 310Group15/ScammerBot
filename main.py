5633b8b16ed30f577d060e3030a396ccac506385
Sol Thiessen <soljthiessen@gmail.com>
2022-11-09 14:48:57 -0800

    updated main to follow dunder main convention

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import chatbotClasses
-cont = True
-while cont:
-    cont = chatbotClasses.ReadInput.read()
+def main():
+    cont = True
+    while cont:
+        cont = chatbotClasses.ReadInput.read()
+
+if __name__ == "__main__":
+    main()
