e81ac870ed8fe73f1d52044f989a95161393fa4e
Sol Thiessen <soljthiessen@gmail.com>
2022-11-14 15:18:00 -0800

    added third test case for probability function

--- chatbotTest.py
+++ chatbotTest.py
@@ -10,5 +10,8 @@ class TestBot(unittest.TestCase):
         self.assertIn(cb.ReadInput.read("how are you?").removeprefix(cb.ReadInput.USERNAME + ": "),rp.HOW_I_AM)
         self.assertNotIn(cb.ReadInput.read("I climb v2, how about you?").removeprefix(cb.ReadInput.USERNAME + ": "),rp.HOW_I_AM)
 
+    def test_probability(self):
+        self.assertEqual(cb.InputAnalysis.probability(["what", "kind", "of", "shoes", "do", "you", "wear"],recognizedWords=["what", "kind", "type", "of", "shoes", "do", "you", "have", "wear"],requiredWords=["shoes"]),int(7/9*100))
+
 if __name__=="__main__":
     unittest.main()
